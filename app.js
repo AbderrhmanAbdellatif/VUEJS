d53d26d2f7180b06d302ab2b6174cdd51e8dc34e
Abdelrahman Abdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2023-09-04 14:08:41 +0300

    computed

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 // Create a new Vue app
 const app = Vue.createApp({
   // Define the data properties
-  data() {
+  computed: {
+    navbarClasses() {
       return {
-        activePage:0,
-        useDarkNavbar:true,
-          // Array of page objects
-          pages: [
-              {
-                  // Nav information
-                  nav: { text: 'Home', url: 'index.html' },
-                  // Page-specific information
-                  pageTitle: 'Hello Vue',
-                  content: 'Welcome to the wonderful world of Vue'
-              },
-              {
-                  // Nav information
-                  nav: { text: 'About', url: 'about.html' },
-                  // Page-specific information
-                  pageTitle: 'About Vue',
-                  content: 'This is the About page'
-              },
-              {
-                  // Nav information
-                  nav: { text: 'Contact', url: 'contact.html' },
-                  // Page-specific information
-                  pageTitle: 'Contact Vue',
-                  content: 'This is the Contact page'
-              }
-          ]
-      };
+        'navbar-dark bg-dark': this.useDarkNavbar,
+        'navbar-light bg-light': !this.useDarkNavbar
+      }
+    }
+  },
+  data() {
+    return {
+      activePage: 0,
+      useDarkNavbar: true,
+      // Array of page objects
+      pages: [
+        {
+          // Nav information
+          nav: { text: 'Home', url: 'index.html' },
+          // Page-specific information
+          pageTitle: 'Hello Vue',
+          content: 'Welcome to the wonderful world of Vue'
+        },
+        {
+          // Nav information
+          nav: { text: 'About', url: 'about.html' },
+          // Page-specific information
+          pageTitle: 'About Vue',
+          content: 'This is the About page'
+        },
+        {
+          // Nav information
+          nav: { text: 'Contact', url: 'contact.html' },
+          // Page-specific information
+          pageTitle: 'Contact Vue',
+          content: 'This is the Contact page'
+        }
+      ]
+    };
   }
 });
 
