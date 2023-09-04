eaa5dfeeed391b7b6f2432c70303f8a1e80195f6
Abdelrahman Abdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2023-09-04 12:02:52 +0300

    Update

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-Vue.createApp({
-    data() {
-        return {
-          pageTitle:'Hello Vue',
-          content:'Welcome to the wonderful worldd of vue'  
-        };
-    },
-}).mount('#content');
-
-Vue.createApp({
+// Create a new Vue app
+const app = Vue.createApp({
+  // Define the data properties
   data() {
       return {
-       links:[
-        {text:'Home',url:'index.html'},
-        {text:'About',url:'about.html'},
-        {text:'Contact',url:'Contact.html'}
-      ]
+        activePage:0,
+          // Array of page objects
+          pages: [
+              {
+                  // Nav information
+                  nav: { text: 'Home', url: 'index.html' },
+                  // Page-specific information
+                  pageTitle: 'Hello Vue',
+                  content: 'Welcome to the wonderful world of Vue'
+              },
+              {
+                  // Nav information
+                  nav: { text: 'About', url: 'about.html' },
+                  // Page-specific information
+                  pageTitle: 'About Vue',
+                  content: 'This is the About page'
+              },
+              {
+                  // Nav information
+                  nav: { text: 'Contact', url: 'contact.html' },
+                  // Page-specific information
+                  pageTitle: 'Contact Vue',
+                  content: 'This is the Contact page'
+              }
+          ]
       };
-  },
-}).mount('nav');
+  }
+});
+
+// Mount the Vue app to a DOM element with id 'app'
+app.mount('#app');
