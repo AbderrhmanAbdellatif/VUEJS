3fcf06f348e31e1c978cd231106b9632abb97cca
Abdelrahman Abdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2023-09-05 15:44:41 +0300

    Update

---
+++ vue-start-spa/src/components/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <!-- Navbar -->
+    <navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => activePage = index">
+    </navbar>
+
+
+
+    <page-viewer :page="pages[activePage]">
+    </page-viewer>
+
+  </div>
+</template>
+<script>
+import PageViewer from './PageViewer.vue'
+import Navbar from './Navbar.vue'
+
+export default {
+  components: {
+    PageViewer,
+    Navbar
+  },
+  data() {
+    return {
+      activePage: 0,
+
+      // Array of page objects
+      pages: [
+        {
+          // Nav information
+          nav: { text: 'Home', url: 'index.html' },
+          // Page-specific information
+          pageTitle: 'Hello Vue',
+          content: 'Welcome to the wonderful world of Vue'
+        },
+        {
+          // Nav information
+          nav: { text: 'About', url: 'about.html' },
+          // Page-specific information
+          pageTitle: 'About Vue',
+          content: 'This is the About page'
+        },
+        {
+          // Nav information
+          nav: { text: 'Contact', url: 'contact.html' },
+          // Page-specific information
+          pageTitle: 'Contact Vue',
+          content: 'This is the Contact page'
+        }
+      ]
+    };
+  }
+
+}
+</script>
